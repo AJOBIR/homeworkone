ac569b608e51a7924e0ed7a6ee3fb199309c1868
User <akobirurenov232@gmail.com>
2021-11-08 10:50:29 +0500

    amen turtinchi oynam  qushdim

---
+++ lib/pege/fourPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class FourPage extends StatefulWidget {
+  const FourPage({Key? key}) : super(key: key);
+
+  @override
+  _FourPageState createState() => _FourPageState();
+}
+
+class _FourPageState extends State<FourPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('bu 4 chi oyna'),
+      ),
+    );
+  }
+}
